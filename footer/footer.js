7e4d08e940a54a4e9f532f6f2ebeac88005e696d
Lucie Sedlackova <sedlacek.lucie@gmail.com>
2025-08-23 21:23:25 +0200

    první commit

---
+++ footer/footer.js
@@ -0,0 +1,14 @@
+(async function(){
+  const mount = document.getElementById('footer-root');
+  if (!mount) return;
+
+  try {
+    const res = await fetch('footer/footer.html', { cache: 'no-cache' });
+    mount.innerHTML = await res.text();
+  } catch(e) {
+    console.error('Nepodařilo se načíst footer:', e);
+  }
+})();
+
+
+
